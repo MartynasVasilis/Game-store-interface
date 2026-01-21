2a4c79dedf963d9b5c6569cc182170ecc29bb9e3
Martynas Vasilis <martynas.vasilis@gmail.com>
2026-01-22 00:40:54 +0200

    DockerIgnore change

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -2,7 +2,7 @@ const path = require("path");
 const Database = require("better-sqlite3");
 
 const dbPath =
-  process.env.DB_PATH || path.join(process.cwd(), "data", "db.sqlite");
+  process.env.DB_PATH || path.join(process.cwd(), "data/db.sqlite");
 
 console.log("Using DB at:", dbPath);
 
